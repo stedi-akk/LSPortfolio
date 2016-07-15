d41464686101f7b2bce98b6119275b944b04e0a3
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2016-07-15 17:06:44 +0100

    correct LoadingActivity

--- app/src/main/java/com/stedi/lsportfolio/App.java
+++ app/src/main/java/com/stedi/lsportfolio/App.java
@@ -2,9 +2,22 @@ package com.stedi.lsportfolio;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Handler;
 
+import java.util.LinkedList;
+
+// TODO ic_launcher
+// TODO build.gradle
+// TODO splash pending activity start
+// TODO correct toolbar shadow
+// TODO no internet toast
 public final class App extends Application {
     private static App instance;
+
+    private final LinkedList<Runnable> pendingRunnables = new LinkedList<>();
+    private final Handler handler = new Handler();
+
+    private boolean isResumed;
 
     @Override
     public void onCreate() {
@@ -14,5 +27,30 @@ public final class App extends Application {
 
     public static Context getContext() {
         return instance.getApplicationContext();
+    }
+
+    public static void postOnResume(Runnable runnable) {
+        if (instance.isResumed)
+            instance.handler.post(runnable);
+        else
+            instance.pendingRunnables.add(runnable);
+    }
+
+    public static void onResume() {
+        instance.isResumed = true;
+        instance.executePendingRunnables();
+    }
+
+    public static void onPause() {
+        instance.isResumed = false;
+    }
+
+    public static boolean isResumed() {
+        return instance.isResumed;
+    }
+
+    private void executePendingRunnables() {
+        while (!pendingRunnables.isEmpty())
+            handler.post(pendingRunnables.pollFirst());
     }
 }
--- app/src/main/java/com/stedi/lsportfolio/ui/activity/LoadingActivity.java
+++ app/src/main/java/com/stedi/lsportfolio/ui/activity/LoadingActivity.java
@@ -4,42 +4,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.stedi.lsportfolio.App;
 import com.stedi.lsportfolio.R;
 import com.stedi.lsportfolio.Utils;
 import com.stedi.lsportfolio.api.ResponseLsAllApps;
 import com.stedi.lsportfolio.api.Server;
 import com.stedi.lsportfolio.model.LsAllApps;
 
-// TODO ic_launcher
-// TODO build.gradle
-// TODO splash pending activity start
-// TODO correct toolbar shadow
-// TODO no internet toast
-public class LoadingActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity implements Runnable {
+    private static Thread loadingThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_activity);
+        if (LsAllApps.getInstance().getApps() != null) {
+            startActivity(new Intent(this, DrawerActivity.class));
+        } else if (savedInstanceState == null && loadingThread == null) {
+            loadingThread = new Thread(this);
+            loadingThread.start();
+        }
+    }
 
-        if (savedInstanceState == null) {
-            new Thread(new Runnable() {
+    @Override
+    public void run() {
+        try {
+            final ResponseLsAllApps response = Server.requestLsAllApps();
+            App.postOnResume(new Runnable() {
                 @Override
                 public void run() {
-                    try {
-                        ResponseLsAllApps response = Server.requestLsAllApps();
-                        LsAllApps.getInstance().setApps(response.getApps());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                startActivity(new Intent(LoadingActivity.this, DrawerActivity.class));
-                            }
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Utils.showToast("server fail");
-                    }
+                    LsAllApps.getInstance().setApps(response.getApps());
+                    startActivity(new Intent(LoadingActivity.this, DrawerActivity.class));
+                    loadingThread = null;
                 }
-            }).start();
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Utils.showToast("server fail");
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        App.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        App.onPause();
     }
 }
