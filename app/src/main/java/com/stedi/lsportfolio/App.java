bcfd708a5088e93689ce782b927969508084b73b
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2016-07-17 13:48:05 +0100

    correct ui for small screens

--- app/src/main/java/com/stedi/lsportfolio/App.java
+++ app/src/main/java/com/stedi/lsportfolio/App.java
@@ -8,8 +8,6 @@ import com.squareup.otto.Bus;
 
 import java.util.LinkedList;
 
-// TODO all apps click test
-// TODO butterknife
 public final class App extends Application {
     private static App instance;
 
